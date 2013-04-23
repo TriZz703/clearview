381103647bd2b14eab05397f6a84a544b4dd88d4
Michael Sparks <mike@sparksm.com>
2013-04-22 21:42:40 -0400

    flexslider

--- src/flexslider.js
+++ src/flexslider.js
@@ -873,24 +873,3 @@
   }  
 
 })(jQuery);
-(function ($) {
-	
-	$(function () {		
-		
-		$('body').on('click', '#launchGallery', function () {
-			$('#gallery').fadeIn(function () {
-				$('.flexslider').css({
-					width: 600
-				}).flexslider({
-					animation: "slide",
-					start: function (slider) {
-						$('body').removeClass('loading');
-					},
-					smoothHeight: true
-				});
-			});
-		});
-		
-	});
-	
-} (jQuery, window, undefined));
